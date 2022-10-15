83a445ce84c7011eb2158ebacb416da376b2df41
Emanueluz <60400509+Emanueluz@users.noreply.github.com>
2022-10-15 16:27:16 -0300

    Add files via upload

--- hemorragias/mostrar_hemorragias.js
+++ hemorragias/mostrar_hemorragias.js
@@ -1,3 +1,16 @@
+lista={ "imagem_att": "ID_000f0bd99.png",
+    "brain":"ID_00fdec745.png",
+    "subdural":"ID_000f0bd99.png",
+    "bone":"ID_00ed02fcc.png",
+    "heatmap":"ID_00fdec745.png"
+      }
+
+
+
+
+
+
+
 function mudarimg(obj, novaimg){
     document.getElementById(obj).src = novaimg; 
 
@@ -6,38 +19,49 @@ function mudarimg(obj, novaimg){
 
 
 function ler_json(arquivo){
-    const json = require(arquivo);
-    console.log(json)
-
-    return json
+    var lista = require([arquivo])
+    
+    console.log(  lista)
+     return lista
 
 };
+function set_imagem_atual2(){
+     const nome_imagem=lista.imagem_att;
+    return nome_imagem;
+ 
 
+
+}
+
+
+function set_imagem_atual(){
+    const imagem_att=lista.imagem_att;
+    const div = document.querySelector("#imagem_atual");
+    mudarimg("imagem_atual",imagem_att)
+
+}
 
 function set_bone(arquivo){
-    const json = require(arquivo);
-    console.log(json.bone+".png")
-    return json.bone+".png";
+    const bone=lista.bone;
+    mudarimg("imagem_mod",bone)
+    
+       
 
 }
 
-function set_brain(arquivo){
+function set_brain(){
+    const brain=lista.brain;
+    const botton =document.querySelector("#botton_brain");
+    mudarimg("imagem_mod",brain)
     
-    const json = require(arquivo);
-
-    return json.brain+".png";
+       
 }
 function set_subdural(arquivo){
-    const json = require(arquivo);
-
-    return json.subdural+".png";
+    const subdural=lista.subdural;
+    mudarimg("imagem_mod",subdural)
 }
 function set_heatmap(arquivo){
-    const json = require(arquivo);
-
-    return json.heatmap+".png";
+    const heatmap=lista.heatmap;
+    mudarimg("imagem_mod",heatmap)
 }
-
-
-var slice = ler_json("C:\\Users\\x\\Desktop\\DesenvolvimentoWeb-main\\hemorragias\\teste.json")
-console.log(set_bone(slice))
+ 
